b2007e546c8cc2d1790df4b90d38da2472d32aac
AmateurECE <edtwardy@mtu.edu>
2017-06-06 18:04:04 -0500

    Code restructure to conform to style guide

--- clinkedlist.c
+++ clinkedlist.c
@@ -1,24 +1,173 @@
-/*
- * NAME: clinkedlist.c -- test script for the implementation of the
- *       circularly linked list in clinkedlist.h
+/*******************************************************************************
+ * NAME: clinkedlist.c
+ *
  * AUTHOR: Ethan D. Twardy
+ *
+ * DESCRIPTION: API functions and test code for the types and prototypes
+ *              outlined in clinkedlist.h; a circularly-linked list.
+ *
  * CREATED: 05/05/17
- * LAST EDITED: 05/05/17
- */
+ *
+ * LAST EDITED: 06/06/17
+ ***/
+
+/*******************************************************************************
+ * INCLUDES
+ ***/
 
 #include <stdio.h>
 #include <stdlib.h>
+
+#ifdef CONFIG_DEBUG_CLIST
 #include <time.h>
 #include <assert.h>
+#endif
 
 #include "clinkedlist.h"
 
-static inline void error_exit(char * msg)
+/*******************************************************************************
+ * LOCAL PROTOTYPES
+ ***/
+
+#ifdef CONFIG_DEBUG_CLIST
+static inline void error_exit(char *);
+#endif
+
+/*******************************************************************************
+ * API FUNCTIONS
+ ***/
+
+/*******************************************************************************
+ * FUNCTION:	    clist_init
+ *
+ * DESCRIPTION:	    Initializes the list in 'list.'
+ *
+ * ARGUMENTS:	    list: (Clist *) -- the list to be initialized.
+ *		    destroy: (void (*)(void *)) -- function pointer to free the
+ *		    data held in a node.
+ *
+ * RETURN:	    void
+ *
+ * NOTES:	    O(1)
+ ***/
+void clist_init(CList * list, void (*destroy)(void *))
 {
-  fprintf(stderr, "%s\n", msg);
-  exit(1);
+  list->head = NULL;
+  list->destroy = destroy;
+  list->size = 0;
 }
 
+/*******************************************************************************
+ * FUNCTION:	    clist_insnxt
+ *
+ * DESCRIPTION:	    Inserts 'data' in a new node placed after 'node.'
+ *
+ * ARGUMENTS:	    list: (Clist *) -- the list to be operated on.
+ *		    node: (CListEmnt *) -- the reference node.
+ *		    data: (void *) -- the data to be inserted.
+ *
+ * RETURN:	    int -- 0 on success, -1 otherwise.
+ *
+ * NOTES:	    O(1)
+ ***/
+int clist_insnxt(CList * list, CListElmt * node, void * data)
+{
+  CListElmt * new_elmt;
+  if ((new_elmt = (CListElmt *)malloc(sizeof(CListElmt))) == NULL)
+    return -1;
+
+  new_elmt->data = data;
+
+  if (clist_isempty(list)) {
+    /* Handle insertion when the list is empty. */
+
+    new_elmt->next = new_elmt;
+    list->head = new_elmt;
+
+  } else {
+    /* Handle insertion when it's not. */
+
+    new_elmt->next = node->next;
+    node->next = new_elmt;
+
+  }
+
+  list->size++;
+  return 0;
+}
+
+/*******************************************************************************
+ * FUNCTION:	    clist_remnxt
+ *
+ * DESCRIPTION:	    Removes the node after 'node' and places its data in 'data.'
+ *
+ * ARGUMENTS:	    list: (Clist *) -- the list to be operated on.
+ *		    node: (CListEmnt *) -- the reference node.
+ *		    data: (void *) -- the data that was removed.
+ *
+ * RETURN:	    int -- 0 on success, -1 otherwise.
+ *
+ * NOTES:	    O(1)
+ ***/
+int clist_remnxt(CList * list, CListElmt * node, void ** pData)
+{
+  if (clist_isempty(list))
+    return -1;
+
+  *pData = node->next->data;
+  CListElmt * old;
+
+  if (node == clist_head(list)) {
+
+    old = list->head->next;
+    list->head->next = old->next;;
+    
+  } else {
+    
+    old = node->next;
+    node->next = node->next->next;
+
+  }
+
+  list->size--;
+  free(old);
+  return 0;
+}
+
+/*******************************************************************************
+ * FUNCTION:	    clist_dest
+ *
+ * DESCRIPTION:	    Removes all nodes and sets all bytes of memory in the CList
+ *		    structure to 0. If 'destroy' is set to NULL, does not
+ *		    destroy the data.
+ *
+ * ARGUMENTS:	    list: (Clist *) -- the list to be destroyed.
+ *
+ * RETURN:	    void.
+ *
+ * NOTES:	    O(n)
+ ***/
+void clist_dest(CList * list)
+{
+  if (clist_isempty(list))
+    return;
+
+  void * pData;
+  while (!clist_isempty(list)) {
+
+    clist_remnxt(list, clist_head(list), (void **)&pData);
+    list->destroy(pData);
+
+  }
+
+  memset(list, 0, sizeof(CList));
+}
+
+/*******************************************************************************
+ * MAIN
+ ***/
+
+#ifdef CONFIG_DEBUG_CLIST
 int main(int argc, char * argv[])
 {
   int * pNum;
@@ -29,7 +178,7 @@ int main(int argc, char * argv[])
 
   srand((unsigned)time(NULL));
 
-  // Populate the list
+  /* Populate the list */
   printf("==== Inserting ====\n");
   for (int i = 0; i < 10; i++) {
     pNum = (int *)malloc(sizeof(int));
@@ -38,7 +187,7 @@ int main(int argc, char * argv[])
     clist_insnxt(list, clist_head(list), (void *)pNum);
   }
 
-  // Remove from the list
+  /* Remove from the list */
   printf("==== Removing =====\n");
   for (int i = 0; i < 10; i++) {
     clist_remnxt(list, clist_head(list), (void **)&pNum);
@@ -51,3 +200,18 @@ int main(int argc, char * argv[])
 
   return 0;
 }
+#endif
+
+/*******************************************************************************
+ * LOCAL FUNCTIONS
+ ***/
+
+#ifdef CONFIG_DEBUG_CLIST
+static inline void error_exit(char * msg)
+{
+  fprintf(stderr, "%s\n", msg);
+  exit(1);
+}
+#endif
+
+/******************************************************************************/
--- clinkedlist.h
+++ clinkedlist.h
@@ -12,8 +12,9 @@
 #include <stdio.h>
 #include <string.h>
 
-/************* TYPE DEFINITIONS *************/
-/********************************************/
+/*******************************************************************************
+ * TYPE DEFINITIONS
+ ***/
 
 typedef struct _CListElmt_ {
 
@@ -30,105 +31,21 @@ typedef struct _CList_ {
 
 } CList;
 
-/************* MACRO DEFINITIONS ************/
-/********************************************/
+/*******************************************************************************
+ * MACRO DEFINITIONS
+ ***/
 
 #define clist_size(list) ((list)->size)
 #define clist_isempty(list) ((list)->size == 0 ? 1 : 0)
 #define clist_head(list) ((list)->head)
 
-/************* PUBLIC INTERFACE *************/
-/********************************************/
+/*******************************************************************************
+ * API FUNCTION PROTOTYPES
+ ***/
 
-// FUNCTION: clist_init O(1) -- initializes the list in list.
-// PARAMETERS: list: (CList *) -- the list to be initialized
-//             destroy: (void (*)(void *)) -- function pointer to free the data in the CListElmt.
-// RETURN: void
-void clist_init(CList * list, void (*destroy)(void *))
-{
-  list->head = NULL;
-  list->destroy = destroy;
-  list->size = 0;
-}
+extern void clist_init(CList *, void (*)(void *));
+extern int clist_insnxt(CList *, CListElmt *, void *);
+extern int clist_remnxt(CList *, CListElmt *, void **);
+extern void clist_dest(CList *);
 
-// FUNCTION: clist_insnxt O(1) -- inserts the data in a new node after node parameter.
-// PARAMETERS: list: (CList *) -- the list to be operated on.
-//             node: (CListElmt *) -- the reference node.
-//             data: (void *) -- the data to be inserted.
-// RETURN: int -- -1 on failure, 0 on success.
-int clist_insnxt(CList * list, CListElmt * node, void * data)
-{
-  CListElmt * new_elmt;
-  if ((new_elmt = (CListElmt *)malloc(sizeof(CListElmt))) == NULL)
-    return -1;
-
-  new_elmt->data = data;
-
-  if (clist_isempty(list)) {
-    // Handle insertion when the list is empty.
-
-    new_elmt->next = new_elmt;
-    list->head = new_elmt;
-
-  } else {
-    // Handle insertion when it's not.
-
-    new_elmt->next = node->next;
-    node->next = new_elmt;
-
-  }
-
-  list->size++;
-  return 0;
-}
-
-// FUNCTION: clist_remnxt O(1) -- remove the data after the reference node parameter.
-// PARAMETERS: list: (CList *) -- the list to be operated on.
-//             node: (CListElmt *) -- the reference node.
-//             pData: (void **) -- data in the removed node is placed here.
-// RETURN: int -- -1 on failure, 0 on success.
-int clist_remnxt(CList * list, CListElmt * node, void ** pData)
-{
-  if (clist_isempty(list))
-    return -1;
-
-  *pData = node->next->data;
-  CListElmt * old;
-
-  if (node == clist_head(list)) {
-
-    old = list->head->next;
-    list->head->next = old->next;;
-    
-  } else {
-    
-    old = node->next;
-    node->next = node->next->next;
-
-  }
-
-  list->size--;
-  free(old);
-  return 0;
-}
-
-// FUNCTION: clist_dest O(n) -- removes all nodes and sets the memory to 0.
-// PARAMETERS: list: (CList *) -- the list to be destroyed.
-// RETURN: void
-void clist_dest(CList * list)
-{
-  if (clist_isempty(list))
-    return;
-
-  void * pData;
-  while (!clist_isempty(list)) {
-
-    clist_remnxt(list, clist_head(list), (void **)&pData);
-    list->destroy(pData);
-
-  }
-
-  memset(list, 0, sizeof(CList));
-}
-
-#endif // __ET_CLINKEDLIST__
+#endif /* __ET_CLINKEDLIST__ */
